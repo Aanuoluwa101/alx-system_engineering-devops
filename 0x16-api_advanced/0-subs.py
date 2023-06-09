0c7826d36467c43334e220f1d24fdb0218dbecab
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-06-09 00:07:56 -0700

    first 3

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Total subscribers for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+    headers = {'User-Agent': 'mercymercy'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    
+    
+    if r.status_code != 200:
+        return 0
+
+    # load response unit from json
+    data = r.json()['data']
+    # extract list of pages
+    pages = data['children']
+    # extract data from first page
+    page_data = pages[0]['data']
+    # return number of subreddit subs
+    return page_data['subreddit_subscribers']
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Query Reddit API to determine subreddit's hot article count
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], next_page=None):
+    """Request subreddit recursively using pagination
+    """
+    
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    # if page specified, pass as parameter
+    if next_page:
+        url += '?after={}'.format(next_page)
+    headers = {'User-Agent': '0x16-api_advanced-mercymercy'}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+
+    # load response unit from json
+    data = r.json()['data']
+
+    # extract list of pages
+    posts = data['children']
+    for post in posts:
+        hot_list.append(post['data']['title'])
+
+    next_page = data['after']
+    if next_page is not None:
+        return recurse(subreddit, hot_list, next_page)
+    else:
+        return hot_list
